79f6a9364b54dd54e2ad8635c9d42a48d80a8bd8
yousrik20 <63603356+yousrik20@users.noreply.github.com>
2021-04-17 17:01:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const spinner = document.querySelector(".spinner");
-document.querySelector("body").style.display = "none";
-window.addEventListener("load", function () {
-  document.querySelector("body").style.display = "block";
-  spinner.style.display = "none";
-});const skillsSection = document.querySelector(".skills");
+// const spinner = document.querySelector(".spinner");
+// document.querySelector("body").style.display = "none";
+// window.addEventListener("load", function () {
+//   document.querySelector("body").style.display = "block";
+//   spinner.style.display = "none";
+// });
+const skillsSection = document.querySelector(".skills");
 const skillsSquare = skillsSection.getBoundingClientRect();
 window.addEventListener("scroll", function () {
   // console.log(skillsSquare.top);
